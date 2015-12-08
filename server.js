9e98c0a82a9611860564ba83080d310a34d81107
atiannicelli <atiannicelli@gmail.com>
2015-12-07 21:06:39 -0500

    fix task to be for dinner

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [{
 	id: 1,
-	description: 'Meet mom for lunch',
+	description: 'Meet mom for dinner',
 	completed: false,
 }, {
 	id: 2,
