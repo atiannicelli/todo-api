fe03adf1389712af6362e35c2b75d2d3961d99c5
atiannicelli <atiannicelli@gmail.com>
2015-12-08 09:02:26 -0500

    added underscore and used pick

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express()
 var PORT = process.env.PORT || 3000;
@@ -18,26 +19,28 @@ app.get('/todos', function (req, res) {
 
 app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo;
-
-	todos.forEach(function (todo) {
-		if (todo.id === todoId) {
-			matchedTodo = todo; 
-		}
-	});
+	var matchedTodo = _.findWhere(todos, {"id": todoId})
 
 	if (matchedTodo) {
 		res.json(matchedTodo); 
 	} else {
 		res.status(404).send(); 
 	}
-})
+});
 
 app.post('/todos', function (req, res) {
-	req.body.id = todoNextId++
-	todos.push(req.body)
-	res.json(req.body)
-})
+	var body = _.pick(req.body, 'description', 'completed')
+	if (!_.isBoolean(body.completed)
+	 || !_.isString(body.description)
+	 || body.description.trim().length === 0) {
+		res.status(400).send();
+		return
+	}
+	body.description = body.description.trim()
+	body.id = todoNextId++
+	todos.push(body)
+	res.json(body)
+});
 
 app.listen(PORT, function () {
 	console.log('Express listening on port ' + PORT + '!');
